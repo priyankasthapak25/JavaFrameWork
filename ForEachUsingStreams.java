eb1d133dc0ecca03034acd2e09eb72f8661a448e
priyankasthapak25 <56381330+priyankasthapak25@users.noreply.github.com>
2019-10-10 11:39:42 +0530

    Create ForEachUsingStreams.java

---
+++ ForEachUsingStreams.java
@@ -0,0 +1,22 @@
+public class Main { 
+
+	public static String frechlop(String[] geek) 
+	{ 
+		int count = 0; 
+		for (String var : geek) { 
+			if (count == 1) 
+				return var; 
+			count++; 
+		} 
+		return ""; 
+	} 
+
+	public static void main(String[] args) 
+		throws Exception 
+	{ 
+
+		String[] arr1 = { "Geeks", "For", "Geeks" }; 
+		String secelt = frechlop(arr1); 
+		System.out.println(secelt); 
+	} 
+} 
